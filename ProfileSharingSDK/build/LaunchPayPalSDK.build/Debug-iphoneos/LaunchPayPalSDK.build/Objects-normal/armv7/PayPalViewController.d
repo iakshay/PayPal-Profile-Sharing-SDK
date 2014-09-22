f2fdf3d0ade1d2a02f4ccd4886ae40eb92494e00
Rohan Aurora <roaurora@ebay.com>
2014-09-22 01:02:59 -0700

    Restore class structure, add folder. Remove extraneous method.

---
+++ ProfileSharingSDK/build/LaunchPayPalSDK.build/Debug-iphoneos/LaunchPayPalSDK.build/Objects-normal/armv7/PayPalViewController.d
@@ -0,0 +1,10 @@
+dependencies: \
+  /Users/roaurora/Documents/GitHub\ Projects/Personal\ Github/Hobby\ Projects/LaunchPayPalSDK/PayPalViewController.m \
+  /Users/roaurora/Documents/GitHub\ Projects/Personal\ Github/Hobby\ Projects/LaunchPayPalSDK/PayPalViewController.h \
+  /Users/roaurora/Documents/GitHub\ Projects/Personal\ Github/Hobby\ Projects/LaunchPayPalSDK/PayPalMobile.h \
+  /Users/roaurora/Documents/GitHub\ Projects/Personal\ Github/Hobby\ Projects/LaunchPayPalSDK/PayPalConfiguration.h \
+  /Users/roaurora/Documents/GitHub\ Projects/Personal\ Github/Hobby\ Projects/LaunchPayPalSDK/PayPalFuturePaymentViewController.h \
+  /Users/roaurora/Documents/GitHub\ Projects/Personal\ Github/Hobby\ Projects/LaunchPayPalSDK/PayPalOAuthScopes.h \
+  /Users/roaurora/Documents/GitHub\ Projects/Personal\ Github/Hobby\ Projects/LaunchPayPalSDK/PayPalPayment.h \
+  /Users/roaurora/Documents/GitHub\ Projects/Personal\ Github/Hobby\ Projects/LaunchPayPalSDK/PayPalPaymentViewController.h \
+  /Users/roaurora/Documents/GitHub\ Projects/Personal\ Github/Hobby\ Projects/LaunchPayPalSDK/PayPalProfileSharingViewController.h
